0fd74571580a380cf76c2c660f346c4304a739cd
paldom <paldomonkos@gmail.com>
2014-08-06 14:12:32 +0200

    ios adID

--- src/ios/CDVUniqueDeviceID.m
+++ src/ios/CDVUniqueDeviceID.m
@@ -5,16 +5,14 @@
 //
 
 #import "CDVUniqueDeviceID.h"
+#import <AdSupport/ASIdentifierManager.h>
 
 @implementation CDVUniqueDeviceID
 
 -(void)get:(CDVInvokedUrlCommand*)command
 {
     [self.commandDelegate runInBackground:^{
-        NSString *uuid = @"TODO";
-
-        // TODO: load UUID from keystore. If empty generate UUID and store.
-        
+        NSString *uuid = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:uuid];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     }];
