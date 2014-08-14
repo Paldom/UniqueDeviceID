1f6990c1952f64a1ba1a0ab880da40b69bba5e0e
paldom <paldomonkos@gmail.com>
2014-08-14 14:31:14 +0200

    ios keychain and userdefaults

--- src/ios/CDVUniqueDeviceID.m
+++ src/ios/CDVUniqueDeviceID.m
@@ -5,14 +5,38 @@
 //
 
 #import "CDVUniqueDeviceID.h"
-#import <AdSupport/ASIdentifierManager.h>
+#import "UICKeyChainStore.h"
 
 @implementation CDVUniqueDeviceID
 
 -(void)get:(CDVInvokedUrlCommand*)command
 {
     [self.commandDelegate runInBackground:^{
-        NSString *uuid = [[[ASIdentifierManager sharedManager] advertisingIdentifier] UUIDString];
+        
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *uuidUserDefaults = [defaults objectForKey:@"uuid"];
+        
+        NSString *uuid = [UICKeyChainStore stringForKey:@"uuid"];
+
+        if ( uuid && !uuidUserDefaults) {
+            [defaults setObject:uuid forKey:@"uuid"];
+            [defaults synchronize];
+            
+        }  else if ( !uuid && !uuidUserDefaults ) {
+            NSString *uuidString = [[NSUUID UUID] UUIDString];
+            
+            [UICKeyChainStore setString:uuidString forKey:@"uuid"];
+            
+            [defaults setObject:uuidString forKey:@"uuid"];
+            [defaults synchronize];
+            
+            uuid = [UICKeyChainStore stringForKey:@"uuid"];
+            
+        } else if ( ![uuid isEqualToString:uuidUserDefaults] ) {
+            [UICKeyChainStore setString:uuidUserDefaults forKey:@"uuid"];
+            uuid = [UICKeyChainStore stringForKey:@"uuid"];
+        }
+
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:uuid];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
     }];
